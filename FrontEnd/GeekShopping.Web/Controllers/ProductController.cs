c5662c2d5767d0ec9305c4f77858e394bca23d35
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2022-08-18 18:34:13 -0300

    GeekProduct-Microservice-frontEnd-Parte2/v1

---
+++ FrontEnd/GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            var product = await productService.FindAllProductModels();
+            return View(product);
+        }
+    }
+}
