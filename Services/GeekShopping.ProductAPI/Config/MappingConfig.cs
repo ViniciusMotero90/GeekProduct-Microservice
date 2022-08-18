7cafd340bcf07664e6f74cede4c8599867721897
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2022-08-18 09:31:05 -0300

    GeekProduct-Microservice/v1

---
+++ Services/GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValuesObjects;
+using GeekShopping.ProductAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ Services/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValuesObjects;
+using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private IMapper _mapper;
+        public ProductRepository(ApplicationDbContext applicationDbContext, IMapper mapper)
+        {
+            _applicationDbContext = applicationDbContext;
+            _mapper = mapper;
+        }
+        public async Task<ProductVO> Create(ProductVO vo)
+        {
+            Product product = _mapper.Map<Product>(vo);
+            _applicationDbContext.Products.Add(product);
+            await _applicationDbContext.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<bool> Delete(long id)
+        {
+            try
+            {
+                Product product = await _applicationDbContext.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+                if (product == null) return false;
+                _applicationDbContext.Products.Remove(product);
+                await _applicationDbContext.SaveChangesAsync();
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _applicationDbContext.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            Product product = await _applicationDbContext.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<ProductVO> Update(ProductVO vo)
+        {
+            Product product = _mapper.Map<Product>(vo);
+            _applicationDbContext.Products.Update(product);
+            await _applicationDbContext.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+    }
+}
