f07562169d0b08968a2d1669f5017a7365fa3988
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2022-08-19 15:30:59 -0300

    GeekProduct-Microservice-frontEnd-Parte6/v1

--- GeekShopping.IdentityServer/Startup.cs
+++ GeekShopping.IdentityServer/Startup.cs
@@ -28,7 +28,7 @@ namespace GeekShopping.IdentityServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<MySqlContext>(options => options.UseMySql("server=localhost;initial catalog=GeekShopping.IndentiyServer;uid=root;pwd=Vinicius23", ServerVersion.Parse("8.0.29-mysql")));
+            services.AddDbContext<MySqlContext>(options => options.UseMySql("server=localhost;initial catalog=GeekShopping.IndentiyServer;uid=root;pwd=", ServerVersion.Parse("8.0.29-mysql")));
             services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<MySqlContext>().AddDefaultTokenProviders();
             var builder = services.AddIdentityServer(options => 
             {
