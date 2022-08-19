644b5276af51dc75605cf7fd4036708263ba9196
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2022-08-19 14:27:52 -0300

    GeekProduct-Microservice-frontEnd-Parte4/v1

---
+++ GeekShopping.IdentityServer/Model/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.IdentityServer.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+    }
+}
---
+++ GeekShopping.IdentityServer/Startup.cs
@@ -0,0 +1,56 @@
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<MySqlContext>(options => options.UseMySql("server=localhost;initial catalog=GeekShopping.IndentiyServer;uid=root;pwd=Vinicius23", ServerVersion.Parse("8.0.29-mysql")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
