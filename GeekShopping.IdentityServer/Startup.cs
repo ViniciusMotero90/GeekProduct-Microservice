5470279f49d4eb8acef92144ee4330f03b96a802
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2022-08-19 15:30:25 -0300

    GeekProduct-Microservice-frontEnd-Parte6/v1

--- GeekShopping.IdentityServer/Startup.cs
+++ GeekShopping.IdentityServer/Startup.cs
@@ -1,6 +1,9 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
 using GeekShopping.IdentityServer.Model.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,7 +28,19 @@ namespace GeekShopping.IdentityServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<MySqlContext>(options => options.UseMySql("server=localhost;initial catalog=GeekShopping.IndentiyServer;uid=root;pwd=", ServerVersion.Parse("8.0.29-mysql")));
+            services.AddDbContext<MySqlContext>(options => options.UseMySql("server=localhost;initial catalog=GeekShopping.IndentiyServer;uid=root;pwd=Vinicius23", ServerVersion.Parse("8.0.29-mysql")));
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<MySqlContext>().AddDefaultTokenProviders();
+            var builder = services.AddIdentityServer(options => 
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+            }).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+            .AddInMemoryClients(IdentityConfiguration.Clients)
+            .AddAspNetIdentity<ApplicationIdentity>();
+            builder.AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +57,8 @@ namespace GeekShopping.IdentityServer
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseIdentityServer();
 
             app.UseAuthorization();
 
